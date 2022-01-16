0eccc1a4e80041111a9842bd8868ac89ab5eff64
Honoki <rkna.clock@gmail.com>
2022-01-16 18:47:03 +0800

    update user system for receiving commands and redeems

---
+++ Twitch Color Game/Assets/_ColorGame/Systems/User System/UserCommandDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+
+namespace ColorGame.User {
+    [Serializable]
+    public class UserCommandDictionary : SerializedMonoBehaviour {
+
+        [SerializeField]
+        private Dictionary<string, UserCommandData> CommandDatas = new Dictionary<string, UserCommandData>();
+
+        public void HandleCommandInvocation(string commandString, bool userIsBroadcaster, bool userIsModerator, string userName, string[] commandArgs) {
+            if (!CommandDatas.ContainsKey(commandString)) {
+                return;
+            }
+
+            var command = CommandDatas[commandString];
+            if (command.IsBroadcasterCommand && !userIsBroadcaster) {
+                return;
+            }
+
+            if (command.IsModeratorCommand && !userIsModerator) {
+                return;
+            }
+
+            command.CommandAction?.Invoke(userName, commandArgs);
+        }
+    }
+
+    [Serializable]
+    public class UserCommandData {
+        public string CommandString;
+        public bool IsBroadcasterCommand;
+        public bool IsModeratorCommand;
+        public UserCommandAction CommandAction;
+    }
+
+    [Serializable]
+    public class UserCommandAction : UnityEvent<string, string[]> { }
+}
---
+++ Twitch Color Game/Assets/_ColorGame/Systems/User System/UserPointRedeemDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorGame.User {
+    public class UserPointRedeemDictionary {
+
+        private Dictionary<string, int> _redeemValueDictionary;
+
+        public UserPointRedeemDictionary() {
+            _redeemValueDictionary = new Dictionary<string, int>();
+            _redeemValueDictionary.Add("CGPoint:500", 500);
+            _redeemValueDictionary.Add("CGPoint:1000", 1000);
+            _redeemValueDictionary.Add("CGPoint:2000", 2000);
+        }
+
+        public bool IsValidRedeemKey(string redeemKey) {
+            return _redeemValueDictionary.ContainsKey(redeemKey);
+        }
+
+        public int GetRedeemValue(string redeemKey) {
+            if (!IsValidRedeemKey(redeemKey)) {
+                return 0;
+            }
+
+            return _redeemValueDictionary[redeemKey];
+        }
+    }
+}
